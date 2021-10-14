6e586e54af26f0697eba5fc4dcb038880e81f641
Kaleb-Aguirre03 <820930@mystma.org>
2021-10-14 08:56:18 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+#define a function to multiply three integers and add one to the product
+def product3Plus1(x, y, z):
+    return x * y * z + 1
+
+#test the function
+print(product3Plus1(3, 4, 5))
